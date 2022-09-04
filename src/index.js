d03fb8df11c9fc77c21d17eb0e03fadc1b8be878
Andrii Popov <popov.andrey80@gmail.com>
2022-09-04 22:13:45 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-const axios = require('axios').default;
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('.search-input'),
+  searchBtn: document.querySelector('.search-btn'),
+};
+
+const KEY = '29711315-8270253fad608a552f88c48ec';
+const BASE_URL = 'https://pixabay.com/api';
+const URL = `${BASE_URL}/?key=${KEY}=cats&image_type&orientation=horizontal&safesearch=true`;
+
+fetch(URL)
+  .then(responce => responce.json().then(data => console.log(data)))
+  .catch(error => console.log(error));
+
+// refs.input.addEventListener('input', enterData);
+
+// function enterData(e) {
+//   e.preventDefault();
+
+//   const inputData = refs.input.value;
+//   fetchUserData(inputData);
+// }
